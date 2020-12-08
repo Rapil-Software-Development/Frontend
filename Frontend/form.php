310683226d5f5f23387a3fda111ec4b200a287b3
Rapil-Software-Deployment <oleksii.hashchenko.it.2017@lpnu.ua>
2020-12-08 23:35:46 +0200

    Commit 6. Index page

--- Frontend/form.php
+++ Frontend/form.php
@@ -3,4 +3,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $login = $_POST['auth_email'];
     $password = $_POST['auth_pass'];
 }
+$key=1;
+if ($key==1) {
+    $filename="index.html";
+}
+$data=file_get_contents($filename);
+echo $data;
 ?>
